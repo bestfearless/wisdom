82dcdd2e1c193d6a389cec19463501a40e7d7c8a
Julian Joe <zhengzhiling0227@icloud.com>
2020-05-20 12:17:48 +0800

    update

--- Scripts/Bjtk.js
+++ Scripts/Bjtk.js
@@ -6,8 +6,5 @@ https?://wx.xuexiguanjia.org/api/user(info)?
 var obj = JSON.parse($response.body);
 obj.data.is_is_vip= 1;
 obj.data.expire_date= 3760000000;
-obj.data.orderStatusNum.switchUserInfo.is_vip= 1;
-obj.data.orderStatusNum.switchUserInfo.expire_date= 3760000000;
-obj.data.orderStatusNum.switchUserInfo.is_vip= 1;
 
 $done({body: JSON.stringify(obj)});
