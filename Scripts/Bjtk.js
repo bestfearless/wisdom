b22fc5aa6abd14e395131bc508bf9d724880855d
Julian Joe <zhengzhiling0227@icloud.com>
2020-05-20 12:22:15 +0800

    update

--- Scripts/Bjtk.js
+++ Scripts/Bjtk.js
@@ -4,7 +4,7 @@ https?://wx.xuexiguanjia.org/api/user(info)?
 */
 
 var obj = JSON.parse($response.body);
-obj.data.is_is_vip= 1;
+obj.data.is_vip= 1;
 obj.data.expire_date= 3760000000;
 
 $done({body: JSON.stringify(obj)});
