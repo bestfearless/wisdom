acbcbdf31d7912f33e83abcee908ccd7723eb4eb
Julian Joe <zhengzhiling0227@icloud.com>
2020-05-21 10:37:47 +0800

    update

--- Scripts/Bjtk.js
+++
@@ -1,9 +0,0 @@
-/*
-Beijingtiku unlock vip
-https?://wx.xuexiguanjia.org/api/user(info)?
-*/
-
-var obj = JSON.parse($response.body);
-obj.data.is_vip= 1;
-obj.data.expire_date= 3760000000;
-$done({body: JSON.stringify(obj)});
--- Scripts/Busuu.js
+++
@@ -1,9 +0,0 @@
-/*
-Busuu unlock
-^https:\/\/api\.busuu\.com\/users\/me*
-*/
-
-var obj = JSON.parse($response.body);
-obj.data.is_premium= true;
-obj.data.access.tier= "plus";
-$done({body: JSON.stringify(obj)});
